f507addbf15b98afb16673b443b0dd7341cf2152
Filip Petkov <filip.petkov.99@gmail.com>
2023-12-12 14:37:10 +0200

    Add project files.

---
+++ Parallel_And_Distributed_Systems_Assignment_B/Painter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallel_And_Distributed_Systems_Assignment_B
+{
+    internal class Painter
+    {
+        public int Id { get; set; }
+        public Dictionary<Circle, bool> CirclesPool { get; set; }
+
+        public Painter(Dictionary<Circle, bool> _CirclesPool, int id)
+        {
+            this.CirclesPool = _CirclesPool;
+            Id = id;
+        }
+
+        
+        public void PaintCircle(Circle circle, SemaphoreSlim locker)
+        {
+            locker.Wait();
+            CirclesPool[circle] = true;
+            Thread.Sleep(20);
+            Console.WriteLine($"Painting a circle {circle.Id} right now by painter {Id}...");
+            locker.Release();
+        }
+
+        public bool IsCirclePainted(List<Painter> painters, Circle circle)
+        {
+            foreach (var painter in painters)
+            {
+                if (painter.CirclesPool[circle] == true) return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Parallel_And_Distributed_Systems_Assignment_B/Program.cs
@@ -0,0 +1,33 @@
+﻿using Parallel_And_Distributed_Systems_Assignment_B;
+
+internal class Program
+{
+    public static SemaphoreSlim locker = new SemaphoreSlim(1, 1);
+    private static void Main(string[] args)
+    {
+
+        CirclesObject CirclesObject = new CirclesObject(100, 3);
+        List<Painter> listOfPainters = new List<Painter>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Painter painter = new Painter(CirclesObject.CirclesPool, i+1);
+            listOfPainters.Add(painter);
+        }
+
+        Parallel.ForEach(listOfPainters, painter =>
+        {
+            while(CirclesObject.CirclesQueue.Count > 0)
+            {
+                CirclesObject.CirclesQueue.TryDequeue(out var circleToPaint);
+
+                if (!painter.IsCirclePainted(listOfPainters, circleToPaint))
+                {
+                    painter.PaintCircle(circleToPaint, locker);
+                }
+            }
+        });
+    }
+
+
+}
